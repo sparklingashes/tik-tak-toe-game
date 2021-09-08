1d88625b0531fd60a9c99eae9b5d40575133f9fe
Ayushi <ayushi.srivastava091@gmail.com>
2021-09-08 22:01:42 +0530

    adding project

---
+++ src/components/Square.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const square = ({ value, onClick, isWinningSquare }) => {
+  return (
+      <button type="button" onClick={onClick}
+      className = {`square ${isWinningSquare ? 'winning' : ' '} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
+      >
+        {value}
+        </button>
+  );
+};
+
+export default square;
